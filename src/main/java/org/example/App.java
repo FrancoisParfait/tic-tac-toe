92bc5f7d013a0ce7e21250988a4c6d693a1ec3a2
Christian Johnson <cartian4@gmail.com>
2023-05-25 06:30:04 -0400

    Add JFrame to App

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,17 @@
 package org.example;
+import javax.swing.*;
 
-public class App 
-{
-    public static void main (String[] args) {
+public class App implements Runnable {
+    @Override
+    public void run() {
 
+        JFrame frame = new JFrame();
+        frame.setResizable(false);
+        frame.setSize(640, 640);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main (String[] args) throws Exception {
+        SwingUtilities.invokeLater(new App());
     }
 }
