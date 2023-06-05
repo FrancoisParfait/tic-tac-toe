c89fcffecc7ff4f7475f21e65b22866355c8197e
Christian Johnson <cartian4@gmail.com>
2023-06-05 05:28:21 -0400

    Add ActionListener interface

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,23 @@
 package org.example;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class App implements Runnable {
+public class App implements Runnable, ActionListener {
+    JFrame frame = new JFrame();
+
     @Override
     public void run() {
 
-        JFrame frame = new JFrame();
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setSize(640, 640);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 
     public static void main (String[] args) throws Exception {
